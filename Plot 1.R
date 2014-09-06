e570f3e66c1325dc4874723fcfe5cd6be0316fc8
yuntaozhou <yuntaozhou@hotmail.com>
2014-09-07 05:44:17 +0800

    Update Plot 1.R

--- Plot 1.R
+++ Plot 1.R
@@ -13,5 +13,5 @@ subprojectone$DwT <- as.POSIXct(subprojectone$DwT)
 # creating Plot 1
 hist(subprojectone$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
 # saving to PNG file
-dev.copy(png,file="plot1.png")
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
